5dab7a333858527f0bb94173e41c3793a438f8ea
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-04-07 18:57:28 -0300

    Chore: Setup inicial

---
+++ src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1 className="text-green-500">Olá devs</h1>
+        <p>Squad 10 - o início.</p>
+        <a
+          className="text-green-600 visited:text-purple-600 no-underline"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
