071e067689d204e56cf78279952649f972c0e571
João Guilherme P de Paula <joaoguilhermegl3@gmail.com>
2022-04-11 19:58:34 -0300

    Feat: Create Content

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,11 @@
 export { default as Login } from './Login';
 export { default as Sidebar } from './Sidebar';
+export { default as UserProfile } from './UserProfile';
 export { default as Profile } from './Profile';
-export { default as CreateProfile } from './CreateProfile';
+export { default as EditProfile } from './EditProfile';
+export { default as Navbar } from './Navbar';
+export { default as Feed } from './Feed';
+export { default as ProfileDetail } from './ProfileDetail';
+export { default as Search } from './Search';
+
+/* Feat:Create content */
