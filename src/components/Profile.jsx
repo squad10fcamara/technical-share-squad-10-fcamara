b2ac04a49d5d9e94e4edd9578b761b3947b9c228
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-04-13 14:35:28 -0300

    Feat: Salvar perfil de outro usuário

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,48 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import { v4 as uuidv4 } from 'uuid';
-// import { client } from '../client';
+import { v4 as uuidv4 } from 'uuid';
 
-const Profile = ({ profile: { postedBy, _id, position } }) => {
+import { client } from '../client';
+import { fetchUser } from '../utils/fetchUser';
+
+const Profile = ({ profile: { postedBy, _id, position, save } }) => {
+  const [postHovered, setPostHovered] = useState(false);
+
   const navigate = useNavigate();
+
+  const user = fetchUser();
+
+  const alreadySaved = !!save?.filter(
+    (item) => item?.postedBy?._id === user?.googleId
+  )?.length;
+
+  const saveProfile = (id) => {
+    if (!alreadySaved) {
+      client
+        .patch(id)
+        .setIfMissing({ save: [] })
+        .insert('after', 'save[-1]', [
+          {
+            _key: uuidv4(),
+            userId: user?.googleId,
+            postedBy: {
+              _type: 'postedBy',
+              _ref: user?.googleId,
+            },
+          },
+        ])
+        .commit()
+        .then(() => {
+          window.location.reload();
+        });
+    }
+  };
 
   return (
     <div className="m-2 mb-4">
       <div
+        onMouseEnter={() => setPostHovered(true)}
+        onMouseLeave={() => setPostHovered(false)}
         onClick={() => navigate(`/profile-detail/${_id}`)}
         className="relative cursor-pointer w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
       >
@@ -23,6 +58,25 @@ const Profile = ({ profile: { postedBy, _id, position } }) => {
           <p className="mt-3 text-xl mx-auto md:text-sm  text-white px-4 py-2 rounded-full bg-accent capitalize">
             {position}
           </p>
+          {alreadySaved ? (
+            <button
+              type="button"
+              className="mt-3 bg-accent opacity-75 hover:opacity-100 text-white font-bold px-3 py-1 text-base rounded-3xl hover:shadow-md outline-none"
+            >
+              {save?.length} Salvo
+            </button>
+          ) : (
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                saveProfile(_id);
+              }}
+              type="button"
+              className="mt-3 bg-accent opacity-75 hover:opacity-100 text-white font-bold px-3 py-1 text-base rounded-3xl hover:shadow-md outline-none"
+            >
+              Salvar
+            </button>
+          )}
         </div>
       </div>
     </div>
