3b5024a7ce9f417f17f0d4bee380df06e8932d38
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-04-13 22:34:09 -0300

    Feat: User Profile

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -67,7 +67,7 @@ const Home = () => {
       </div>
       <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
         <Routes>
-          <Route path="/user-profile:userId" element={<UserProfile />} />
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
           <Route path="/*" element={<Content user={user && user} />} />
         </Routes>
       </div>
